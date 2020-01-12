48e66c9ca7b22af073579da29c5152502ad41e07
Tomasz Korenberg <info@casestudio.pl>
2020-01-12 16:24:12 +0100

    add getUser

--- api/db/models/UserModel.js
+++ api/db/models/UserModel.js
@@ -2,21 +2,28 @@ const connection = require("../connection.js");
 const tableName = "users";
 
 const insertUser = () => {
-  const sql = `
+    const sql = `
       INSERT INTO ${tableName}
       VALUES (
           DEFAULT, 'email@test.pl', 'password_salt', 'password_hash', 'imie', 'nazwisko', 'polska', 'warszawa', '00-000', 'region', 'ulica', '11', '7a', POINT(32.3, 12.4), 'gender'
       )
       `;
-          return connection.query(sql);
+    return connection.query(sql);
 };
 
 const getUsers = () => {
     const sql = `
     SELECT * FROM ${tableName};
     `;
-    return connection.query(sql);
+    return connection.query(sql).then((response) => response.rows);
+};
+
+const getUser = (id) => {
+    const sql = `
+    SELECT * FROM ${tableName} WHERE id = ${id};
+    `;
+    return connection.query(sql).then((response) => response.rows);
 };
 
 
-insertUser();
+module.exports = {getUsers, getUser};
