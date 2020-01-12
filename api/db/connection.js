8b67d55e3c0ec8f933087b4c069c036cf78a694c
DamianBorawski <damian.borawski@gmail.com>
2020-01-12 14:08:21 +0100

    database config

--- api/db/connection.js
+++ api/db/connection.js
@@ -1,24 +1,7 @@
-const { Pool, Client } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: '104.155.98.249',
-  database: 'postgres',
-  password: 'react',
-  port: 5432,
-})
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-const client = new Client({
-  user: 'postgres',
-  host: '104.155.98.249',
-  database: 'postgres',
-  password: 'react',
-  port: 5432,
-})
-client.connect()
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+const { Client } = require('pg')
+const config = require('../config.json')
+
+const client = new Client(config.DB);
+client.connect();
+
+module.exports = client;
