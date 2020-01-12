180ff07c6c51cf8b48559b7b4aae50b434a0caa5
Tomasz Korenberg <info@casestudio.pl>
2020-01-12 17:01:58 +0100

    react router first try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <header>
+            <Link to={"/"}>Do niczego</Link>
+            <Link to={"/test"}>Do testu</Link>
+        </header>
+        <main>
+            <Switch>
+                <Route exact path={"/"}>
+                    <h1>Do niczego</h1>
+                </Route>
+                <Route exact path={"/test"}>
+                    <h1>Teeeeest</h1>
+                </Route>
+            </Switch>
+        </main>
+    </Router>
   );
 }
 
