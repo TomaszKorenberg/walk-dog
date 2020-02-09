6f84c59dd8da3aec83a24b2bc6447cb8e70a8dc8
Aleksandra Ku <alexku12@icloud.com>
2020-02-09 12:48:25 +0100

    loginPage

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => (
-    <div></div>
+
+    
+    
+    <div>
+        <div>Login</div>
+        <div>
+            <label htmlFor="login_field">Email address</label>
+            <input type="text" id="login_field" name="login"/>
+            <label htmlFor="password_field">Password</label>
+            <input type="text" id="password_field" name="password"/>
+            <input type="submit"/>
+        </div>
+        <div>
+            New to Walk Dog? <a href="/register">Sing up!</a>
+        </div>
+    </div>
 
 );
 
