6653ace43aaab4c3dc91a755993730d6f41c886b
Tomasz Korenberg <info@casestudio.pl>
2020-02-09 11:43:47 +0100

    basic styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 const Footer = () => (
     <footer>
-        <div>
-
+        <div className={"footer-div"}>
+        To jest footer
         </div>
     </footer>
 );
