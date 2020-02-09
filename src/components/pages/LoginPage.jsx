43be4b49f70128c491f6888258f2efa19f71cf08
zeglarz <zeglarz@gmail.com>
2020-02-09 13:50:11 +0100

    add WalksPage

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,23 +1,8 @@
-import "./LoginPage.scss";
 import React from "react";
-import {BrowserRouter as Router, Link} from "react-router-dom";
-
 
 const LoginPage = () => (     
  
-    <fieldset>
-    <legend>Zaloguj się</legend>
-        <form action="">
-            <input type="text" name="email"/>
-            
-            <input type="text" name="password"/>
-            
-            <button>Zaloguj</button>
-
-
-        </form>
-        </fieldset>
-   
+<div></div>
 );
 
 export default LoginPage;
