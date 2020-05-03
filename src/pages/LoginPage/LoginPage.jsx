2f26483421ce90de6dbb271f8502fd2e5152f576
pauwan <bronchala@gmail.com>
2020-05-03 23:55:34 +0200

    add some css in LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 import {setToken} from "../../utils/token";
 import InfoModal from "../../components/InfoModal/InfoModal";
 import Api from "../../api/api";
-import lock from "../../assets/img/lock.svg";
 import styles from "./LoginPage.module.scss";
+// import eye from "../../assets/img/eye.svg";
 
 const api = new Api();
 
@@ -60,47 +60,63 @@ const LoginPage = () => {
 
     return (
         <>
-            <div  className ={styles.main}>
-                <div className ={styles.lockLogin}>
-                    <img alt={"lock"}
-                         src={lock}
-                         className={styles.lock}
-                    />                
-                    <div className = {styles.login}>Login</div>
-                </div> 
-               
-                <div>
-                    <label htmlFor="email_field">Email address</label>
-                    <input
-                        type="text"
-                        id="email_field"
-                        name="email"
-                        onChange={onEmailChange}
-                    />
-                    {isEmailValid ? null : <p style={{color: "red"}}>check your email adress and password</p>}
-                    <label htmlFor="password_field">Password</label>
-                    <input
-                        type="password"
-                        id="password_field"
-                        name="password"
-                        onChange={onPasswordChange}
-                    />
-                    <button
-                        onClick={onSubmit}>
-                        Sing in
-                    </button>
-                    {
-                        //fixme: powinien się wyświetlac jak jest bledny login lub hasło a wyswietla sie tylko przy blednym loginie
-                    }
-                    {registrationState === true ? null : InfoModal("Nie masz konta?",
-                        "Zarejestruj się!",
-                    "Rejestracja",
-                    "./register")}
-                </div>
-                <div className={styles.end}>
+            <div  className ={styles.wrap}>                
+                    <div className ={styles.flex}>  
+                        <div className={styles.lock}></div>                    
+                        <p className = {styles.login}>Login</p>  
+                    </div>                    
+                    <div>
+                        <div  className = {styles.flex}>
+                            <label htmlFor="email_field">Email address</label>
+                            <input
+                                type="text"
+                                id="email_field"
+                                name="email"
+                                onChange={onEmailChange}
+                                placeholder="mail@gmail.com" required
+                            />
+                            {isEmailValid ? null : <p style={{color: "red"}}>check your email adress and password</p>}
+                        </div>   
+                        <div  className = {styles.flex}> 
+                            <label htmlFor="password_field">Password</label>
+                            <input
+                                type="password"
+                                id="password_field"
+                                name="password"
+                                onChange={onPasswordChange}
+                                className = {styles.eye}
+                            />
+                                {/* <img alt={"eye"}
+                                src={eye}
+                                className={styles.eye}
+                                />     */}
+                                {
+                                     //fixme: obsługa podglądu wpisanego hasła
+                                }
+                        </div>   
+                        <div className={styles.flex}>
+                            <span>Forgot Password ? </span>
+                            <button
+                                onClick={onSubmit}>
+                                Sing in
+                            </button>
+                        </div>
+                        {
+                            //fixme: powinien się wyświetlac jak jest bledny login lub hasło a wyswietla sie tylko przy blednym loginie
+                           
+                        }
 
-                    New to Walk Dog? <a href="/register">Sing up!</a>
-                </div>
+                    
+                        {registrationState === true ? null : InfoModal("Nie masz konta?",
+                            "Zarejestruj się!",
+                        "Rejestracja",
+                        "./register")}
+                    </div>
+                    <div className={styles.end}>
+
+                      <p> New to Walk Dog?  <a href="/register">Sing up!</a></p>
+                    </div>
+                
             </div>
         </>
     )
